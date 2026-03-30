9701b2aec8ad436a99f4b6e79d6783d20dec5c30
Corbind88 <daewood.corbin@student.oaklandcc.edu>
2026-03-30 14:40:15 -0400

    add terraform IaC

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "api" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+
+  key_name = "your-key"
+
+  tags = {
+    Name = "api-deployment"
+  }
+}
+
+resource "aws_security_group" "api_sg" {
+  name = "api_sg"
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
