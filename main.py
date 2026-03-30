3e0b16df7d490a9cb155ff640f6dfb5f85744ac7
Corbind88 <daewood.corbin@student.oaklandcc.edu>
2026-03-30 15:04:56 -0400

    test auto deploy

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ async def word_finder(req: WordFinderRequest):
 
 @app.get("/health")
 def health():
-    return {"status": "updated"}
+    return {"status": "ok"}
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000) 
