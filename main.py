e1063f440356d25981f46b587794048cc76350e4
Corbind88 <daewood.corbin@student.oaklandcc.edu>
2026-03-30 14:45:17 -0400

    test auto deploy

--- main.py
+++ main.py
@@ -135,4 +135,4 @@ def health():
     return {"status": "ok"}
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000) 
